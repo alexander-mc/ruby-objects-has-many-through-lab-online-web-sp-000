a91500c0728654c5807053e89a291dfbf6278c02
Alexander Clamor <alexander.clamor@gmail.com>
2020-08-28 00:44:44 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -25,51 +25,51 @@ class Doctor
 end
 
 
-class Appointment
-  attr_reader :date, :patient, :doctor
-  @@all = []
+# class Appointment
+#   attr_reader :date, :patient, :doctor
+#   @@all = []
   
-  def initialize(date, patient, doctor)
-    @date = date
-    @patient = patient
-    @doctor = doctor
-    @@all << self
-  end
+#   def initialize(date, patient, doctor)
+#     @date = date
+#     @patient = patient
+#     @doctor = doctor
+#     @@all << self
+#   end
   
-  def self.all
-    @@all
-  end
-end
+#   def self.all
+#     @@all
+#   end
+# end
 
 
-class Patient
-  attr_reader :name
-  @@all = []
+# class Patient
+#   attr_reader :name
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    @@all << name
-  end
+#   def initialize(name)
+#     @name = name
+#     @@all << name
+#   end
   
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
   
-  def new_appointment(date, doctor)
-    Appointment.new(date, self, doctor)
-  end
+#   def new_appointment(date, doctor)
+#     Appointment.new(date, self, doctor)
+#   end
   
-  def appointments
-    Appointment.all.select{|appt| appt.patient == self}
-  end
+#   def appointments
+#     Appointment.all.select{|appt| appt.patient == self}
+#   end
 
-  def doctors
-    self.appointments.map{|appt| appt.doctor}
-  end
-end
+#   def doctors
+#     self.appointments.map{|appt| appt.doctor}
+#   end
+# end
 
-doctor_who = Doctor.new("Dr. Who")
-stacey = Patient.new("Stacey")
-zero = Patient.new('Zero')
-good = Patient.new('Good')
-puts Patient.all
+# doctor_who = Doctor.new("Dr. Who")
+# stacey = Patient.new("Stacey")
+# zero = Patient.new('Zero')
+# good = Patient.new('Good')
+# puts Patient.all
